d5d051b307b219ccf8b1e9cb40313c9fbf076dce
Andrew Ngui <andrew.ngui@mail.utoronto.ca>
2022-07-24 15:40:39 -0400

    climbing stairs

---
+++ climbingStairs.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/climbing-stairs/
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        if n == 1 or n == 2:
+            return n
+    
+        lst = []
+        lst.append(1)
+        lst.append(2)
+        
+        for i in range(2,n+1):
+            lst.append(lst[i-1] + lst[i-2])
+        return lst[n-1]
+            
+        
+        
